784b3ecb85e2fecb6e94d4746b1c496af7088d52
SimonBresolin9 <simon.bresolin@gmail.com>
2015-11-06 17:51:33 +0100

    javadoc et methode de joueur
    maj de la javadoc et des méthodes de Joueur sauf celles dépendantes des
    autres classes.

--- src/main/java/fr/bragabresolin/menhir/Joueur.java
+++ src/main/java/fr/bragabresolin/menhir/Joueur.java
@@ -1,32 +1,128 @@
 package fr.bragabresolin.menhir;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-
+/**
+ * 
+ * @author simon
+ *
+ */
 public abstract class Joueur {
 
-	/*
-	 * (non-javadoc)
+	/**
+	 * Représente l'âge du joueur.
+	 * Doit être plus grand que 8. 
 	 */
 	private int age;
+	
+	/**
+	 * Représente le nombre de points présents sur la carte point.
+	 * Ce sont les points calculés à la fin de chaque manche, ils ne peuvent pas¨
+	 * être enlevés par un autre joueur. 
+	 */
 	private int points;
+	
+	/**
+	 * Représente le nombre de graines sur la carte champ.
+	 */
 	private int nombreGraines;
+	
+	/**
+	 * Représente le nombre de menhirs adultes sur la carte champ.
+	 */
 	private int nombreMenhirs;
+	
+	/**
+	 * Représente le nombre de graines protégées par une carte chien active.
+	 * Si aucune carte chien n'est active, ce champ est à 0.
+	 */
 	private int nombreGrainesProteges;
 
 	/**
-	 *
+	 * Représente la main du joueur. Cette main contient à la fois des cartes
+	 * ingrédients et alliés.
 	 */
 	private Collection<Carte> carte;
-
+	
+	
+	/**
+	 * Crée un joueur. Initialise tous ses champs.
+	 */
 	public Joueur() {
+		this.nombreGraines = 0;
+		this.nombreMenhirs = 0;
+		this.nombreGrainesProteges = 0;
+		this.points = 0;
+		this.carte = new ArrayList<Carte>();
 	}
 
-	public void augmenterGraines(int n) {}
-	public void diminuerGraines(int n) {}
-	public void augmenterMehnirs(int n) {}
-	public void diminuerMenhirs(int n) {}
-	public void subirVolGraines(int n) {}
+	/**
+	 * Augmente les graines du joueur de sa carte champ.
+	 * 
+	 * @param n Le nombre de graines à incrémenter du champ du joueur.
+	 */
+	public void augmenterGraines(int n) {
+		if (n <= 0) return;
+		
+		this.nombreGraines += n;
+	}
+	
+	/**
+	 * Diminue les graines de la carte champ du joueur.
+	 * 
+	 * @param n Le nombre de graines à décrémenter du champ du joueur.
+	 * @return Le nombre effectif de graines enlevées.
+	 */
+	public int diminuerGraines(int n) {
+		if (n <= 0) return 0;
+		
+		int grainesAvant = this.nombreGraines;
+		this.nombreGraines -= n;
+		if (this.nombreGraines < 0) {
+			this.nombreGraines = 0;
+		}
+		return grainesAvant - this.nombreGraines;	
+	}
+	
+	/**
+	 * Augmente les menhirs de la carte champ du joueur.
+	 * 
+	 * @param n
+	 */
+	public void augmenterMehnirs(int n) {
+		if (n <= 0) return;
+		
+		this.nombreMenhirs += n;
+	}
+	
+	/**
+	 * Diminue les menhirs de la carte champ du joueur.
+	 * 
+	 * @param n Le nombre de menhirs à décrémenter du champ du joueur.
+	 * @return Le nombre effectif de menhirs enlevés.
+	 */
+	public int diminuerMenhirs(int n) {
+		if (n < 0) return 0;
+		
+		int menhirsAvant = this.nombreMenhirs;
+		this.nombreMenhirs -= n;
+		if (this.nombreMenhirs < 0) {
+			this.nombreMenhirs = 0;
+		}
+		return menhirsAvant - this.nombreMenhirs;	
+	}
+	
+	/**
+	 * Diminue le nombre de graines du joueur lorsque ce dernier subit un vol.
+	 * Prend en compte les graines protégées par un éventuel chien actif.
+	 * 
+	 * @param n Le nombre de graines à voler.
+	 * @return Le nombre de graines effectivement enlevées.
+	 */
+	public int subirVolGraines(int n) {
+		return this.diminuerGraines(n - this.nombreGrainesProteges);
+	}
 
 	public void jouerDansTourAdverse() {
 
