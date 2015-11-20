dde92764a579d941fd0eef46d29416324a213bde
Logan Braga <braga.logan@gmail.com>
2015-11-20 21:22:22 +0100

    Implémentation TransformateurStrategy

--- src/main/java/fr/bragabresolin/menhir/TransformateurStrategy.java
+++ src/main/java/fr/bragabresolin/menhir/TransformateurStrategy.java
@@ -6,11 +6,64 @@ public class TransformateurStrategy implements ComportementStrategy {
 
 	public CarteIngredient choisirCarteIngredient(Joueur joueur, Saison saisonActuelle,
 			ArrayList<Joueur> contexte, ArrayList<CarteIngredient> cartes) {
-		return null;
+
+		CarteIngredient carteChoisie;
+		ActionIngredient action;
+
+		// Si on a des graines, on les fait pousser
+		if (joueur.getNombreGraines() >= 1)
+			action = ActionIngredient.ENGRAIS;
+		// Sinon on en prends d'autres
+		else
+			action = ActionIngredient.GEANT;
+
+		// On choisit la carte avec le meilleur effet voulu
+		// c'est-à-dire la carte la plus proche du nombre de graines si c'est un
+		// engrais
+		CarteIngredient meilleurEffet = null;
+		// Si on prends des graines, on en veut le plus possible
+		for (CarteIngredient carte : cartes) {
+			if (meilleurEffet == null) meilleurEffet = carte;
+			else if (carte.getMatrice().get(saisonActuelle).get(action)
+					> meilleurEffet.getMatrice().get(saisonActuelle).get(action)) {
+				meilleurEffet = carte;
+				if (action == ActionIngredient.GEANT
+						&& meilleurEffet.getMatrice().get(saisonActuelle).get(action) == joueur.getNombreGraines())
+					break;
+			}
+		}
+		carteChoisie = meilleurEffet;
+		carteChoisie.setAction(action);
+		carteChoisie.setOrigine(joueur);
+
+		return carteChoisie;
 	}
 
 	public CarteAllie choisirCarteAllie(Joueur joueur, Saison saisonActuelle,
 			ArrayList<Joueur> contexte, ArrayList<CarteAllie> cartes) {
+		// Vu que cette stratégie transforme les graines le plus tôt possible
+		// on a rarement besoin de les protéger avec un chien.
+		// On donne donc la priorité aux taupes géantes.
+		for (CarteAllie carte : cartes) {
+			if (carte.getAction() == ActionAllie.CHIEN) {
+				if (carte.getMatrice().get(saisonActuelle) > 2
+						&& carte.getMatrice().get(saisonActuelle) <= joueur.getNombreGraines()) {
+					carte.setOrigine(joueur);
+					return carte;
+				}
+			} else {
+				Joueur meilleurJoueur = new JoueurVirtuel("", 8);
+				for (Joueur j : contexte)
+					if (j != joueur)
+						if (j.getNombreMenhirs() > meilleurJoueur.getNombreMenhirs())
+							meilleurJoueur = j;
+				if (carte.getMatrice().get(saisonActuelle) > 1
+						&& meilleurJoueur.getNombreMenhirs() >= carte.getMatrice().get(saisonActuelle)) {
+					carte.setCible(meilleurJoueur);
+					return carte;
+				}
+			}
+		}
 		return null;
 	}
 
